f9a83e7be68f34e0ba26eed49e489fb6f1b785d9
chargecyber <edomkyc777@gmail.com>
2023-06-04 09:54:13 +0900

    charset encoding change

--- ingest.py
+++ ingest.py
@@ -4,12 +4,12 @@ root_directory = './'  # Specify the root directory path here
 data_directory = os.path.join(root_directory, 'data')  # Path to the "data" directory
 output_file_path = os.path.join(root_directory, 'ingest.txt')  # Path to the output file
 
-with open(output_file_path, 'w') as output_file:
+with open(output_file_path, 'w', encoding='utf-8') as output_file:
     # Iterate over all files in the "data" directory
     for filename in os.listdir(data_directory):
         file_path = os.path.join(data_directory, filename)
         if os.path.isfile(file_path):  # Check if it's a file
-            with open(file_path, 'r', encoding='utf-8') as file:
+            with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
                 text = file.read()
                 output_file.write(text)
                 output_file.write('\n')  # Add a newline after each file's content
